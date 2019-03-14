7c7c7be5a98efa750ebabe60995eff31847195dc
Saira2018 <si_chaudhry@sky.com>
2019-03-14 09:25:44 +0000

    removed index.js from master

--- twittersc/src/index.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var server = http.createServer(function (request, response){
-    console.log("server launched");
-    response.write('started');
-    response.end();
-});
-
-server.listen(3000);
